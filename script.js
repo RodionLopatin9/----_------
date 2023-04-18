4660a874f1ed97e43770a726742a2ff5500a415f
Mikhail Sklyarov <mikhail.29.06.2001@gmail.com>
2023-04-18 19:45:23 +0300

    big update

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let offset = 0;
 const sliderLine = document.querySelector('.slider-line')
 
 document.querySelector('.slider-v').addEventListener('click', function(){
-    offset = offset + 320;
-    if(offset > 1280)
+    offset = offset + 800;
+    if(offset > 1600)
     {
         offset = 0;
     }
@@ -11,10 +11,10 @@ document.querySelector('.slider-v').addEventListener('click', function(){
 })
 
 document.querySelector('.slider-n').addEventListener('click', function(){
-    offset = offset - 320;
+    offset = offset - 800;
     if(offset < 0)
     {
-        offset = 1280;
+        offset = 1600;
     }
     sliderLine.style.left = -offset + 'px'; 
 })
@@ -31,4 +31,25 @@ function scrollTop_(){
         top: 0,
         behavior: "smooth"
     })
+}
+
+
+
+
+var modal = document.getElementById("my_modal");
+var btn = document.getElementById("btn_modal_window");
+var span = document.getElementsByClassName("close_modal_window")[0];
+
+btn.onclick = function () {
+   modal.style.display = "block";
+}
+
+span.onclick = function () {
+   modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+   if (event.target == modal) {
+       modal.style.display = "none";
+   }
 }
